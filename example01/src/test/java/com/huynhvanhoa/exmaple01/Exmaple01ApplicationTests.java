3b71bc2a34001e2876e12767fb9dad988f6a056e
Huỳnh Văn Hòa <hoavanpm2021@gmail.com>
2025-01-16 09:32:03 +0700

    Add example01 folder

---
+++ example01/src/test/java/com/huynhvanhoa/exmaple01/Exmaple01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynhvanhoa.exmaple01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exmaple01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
